a50bdb03f42e4edcbca5b71dd6b2ea2dce2237ff
jlengstorf <jason.lengstorf@copterlabs.com>
2015-10-29 17:20:43 +0700

    feat(crud): major refactor so this component reacts and affects expenses
    This component now has editing and deletion capabilities, and changes
    to the app stores will cause updates.

--- js/components/Expenses/Expense.js
+++ js/components/Expenses/Expense.js
@@ -1,38 +1,213 @@
+/* @flow */
 'use strict';
 
 // libs
-import React from 'react';
+import React, {Component} from 'react';
 import moment from 'moment';
+import classnames from 'classnames';
 import {formatMoney} from 'accounting';
 import debug from 'debug';
+
+// components
+import ExpenseData from './ExpenseData';
+import Input from '../Form/Input';
+import Select from '../Form/Select';
+
+// dispatcher
+import {dispatch} from '../../dispatcher';
 
 // debugger
 const log = debug('components/Expense');
 
-export default function Expense(props) {
-  const {
-    expense,
-    categories,
-    people,
-  } = props;
+// helper function
+function _delete(event) {
+  log(event);
+  event.preventDefault();
+  dispatch({
+    type: 'expense/delete',
+    id: event.target.getAttribute('data-id'),
+  });
+}
 
-  people.forEach(log);
+// types
+type Props = {
+  expense: Expense,
+  categories: Map,
+  people: Map,
+};
 
-  const date = moment(expense.date).format('MMM D, YYYY');
-  const category = categories.get(expense.category);
-  const person = people.get(expense.personID);
+type State = {
+  expense: Expense,
+  isEditing: boolean,
+};
 
-  log(`date: ${date}`);
-  log(person);
+// constants
+const ENTER_KEY_CODE = 13;
 
-  return (
-    <li>
-      <ul>
-        <li>{date}</li>
-        <li>{formatMoney(expense.amount)}</li>
-        <li>{expense.vendor}</li>
-        <li>{person.fname}</li>
-      </ul>
-    </li>
-  );
+export default class Expense extends Component<{}, Props, State> {
+
+  constructor(props: Props): void {
+    super(props);
+
+    this.state = {
+      expense: this.props.expense,
+      isEditing: false,
+    };
+  }
+
+  render() {
+    const {
+      categories,
+      people,
+    } = this.props;
+
+    const date = moment(this.state.expense.date).format('MMM D, YYYY');
+    const category = categories.get(this.state.expense.categoryID);
+    const person = people.get(this.state.expense.personID);
+
+    /* jscs:disable disallowQuotedKeysInObjects */
+    const classes = classnames({
+      'expenses__item': true,
+      'expense': true,
+      'expense--editing': this.state.isEditing,
+    });
+    /* jscs:enable disallowQuotedKeysInObjects */
+
+    return (
+      <li className={classes} onDoubleClick={this._onDoubleClick.bind(this)}>
+        <ul className="expense__list">
+          <li className="expense__item">
+            <span className="expense__data-display">
+              {moment(this.state.expense.date).format('MMM D')}
+            </span>
+            <Input
+              name="date"
+              inputClass="expense__input"
+              value={moment(this.state.expense.date).format('YYYY-MM-DD')}
+              changeHandler={this._onChange.bind(this)}
+              keyDownHandler={this._onKeyDown.bind(this)}
+            />
+          </li>
+          <li className="expense__item">
+            <span className="expense__data-display">
+              {formatMoney(this.state.expense.amount)}
+            </span>
+            <Input
+              name="amount"
+              inputClass="expense__input"
+              type="number"
+              value={this.state.expense.amount}
+              changeHandler={this._onChange.bind(this)}
+              keyDownHandler={this._onKeyDown.bind(this)}
+            />
+          </li>
+          <li className="expense__item">
+            <span className="expense__data-display">
+              {this.state.expense.vendor}
+            </span>
+            <Input
+              name="vendor"
+              inputClass="expense__input"
+              value={this.state.expense.vendor}
+              changeHandler={this._onChange.bind(this)}
+              keyDownHandler={this._onKeyDown.bind(this)}
+            />
+          </li>
+          <li className="expense__item">
+            <span className="expense__data-display">
+              <span className="sr-only">{category.name}</span>
+              <i className={`fa fa-${category.icon} expense__category-icon`}></i>
+            </span>
+            <Select
+              name="categoryID"
+              inputClass="expense__input"
+              value={category.id}
+              options={categories}
+              changeHandler={this._onChange.bind(this)}
+            />
+          </li>
+          <li className="expense__item">
+            <span className="expense__data-display">
+              {person.fname}
+            </span>
+            <Select
+              name="personID"
+              inputClass="expense__input"
+              value={person.id}
+              options={people}
+              changeHandler={this._onChange.bind(this)}
+            />
+          </li>
+          <li className="expense__item">
+            <a
+              href="#"
+              className="expense__edit-control"
+              onClick={this._toggleEditing.bind(this)}
+            >
+              <span className="sr-only">{(this.state.isEditing) ? 'save' : 'edit'}</span>
+              <i className={`fa fa-${!this.state.isEditing ? 'pencil' : 'check'}`} />
+            </a>
+            <a
+              href="#"
+              className="expense__edit-control"
+              data-id={this.state.expense.id}
+              onClick={_delete}
+            >
+              <span className="sr-only">delete</span>
+              <i className="fa fa-times-circle-o" />
+            </a>
+          </li>
+        </ul>
+      </li>
+    );
+  }
+
+  _onChange(event: SyntheticEvent): void {
+    let {name, value} = event.target;
+
+    // Parses the date into a UTC timestamp
+    if (name === 'date') {
+      value = +moment(value, 'YYYY-MM-DD');
+    }
+
+    // Makes sure the amount is saved as a number, not a string
+    if (name === 'amount') {
+      value = +value;
+    }
+
+    this.setState(({expense}) => ({
+      expense: expense.set(name, value),
+    }));
+  }
+
+  _toggleEditing(): void {
+
+    // Saves the updated expense if we were editing
+    if (this.state.isEditing) {
+      dispatch({
+        type: 'expense/update',
+        id: this.state.expense.id,
+        expense: this.state.expense,
+      });
+    }
+
+    this.setState({isEditing: !this.state.isEditing});
+  }
+
+  _disableEditing(): void {
+    this.setState({isEditing: false});
+  }
+
+  _onKeyDown(event): void {
+    if (event.keyCode === ENTER_KEY_CODE) {
+      this._toggleEditing();
+    }
+  }
+
+  _onDoubleClick(): void {
+    if (!this.state.isEditing) {
+      this._toggleEditing();
+    }
+  }
+
 }

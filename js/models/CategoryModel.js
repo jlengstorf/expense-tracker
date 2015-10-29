af3982fbb2fe60834e78d6690eb7d14c3599e29b
jlengstorf <jason.lengstorf@copterlabs.com>
2015-10-29 17:14:59 +0700

    feat(icons): added an icon property for categories

--- js/models/CategoryModel.js
+++ js/models/CategoryModel.js
@@ -7,12 +7,14 @@ import {Record} from 'immutable';
 const CategoryRecord = Record({
   id: undefined,
   name: undefined,
+  icon: undefined,
   split: undefined,
 });
 
 export default class Category extends CategoryRecord {
   id: string;
   name: number;
+  icon: string;
   split: array;
 
   constructor(category: object) {
@@ -21,6 +23,7 @@ export default class Category extends CategoryRecord {
       // id: uuid.v4(),
       id: category.id,
       name: category.name,
+      icon: category.icon,
       split: category.split,
     });
   }

9466fe8f777513d75437137df15f0c9beee57b56
jlengstorf <jason.lengstorf@copterlabs.com>
2015-10-30 19:41:20 +0700

    feat(grouping): added date banners to simplify display
    Followed Mint's example and grouped expenses by day to save space
    on line items. Also added CSS transition support.

--- js/components/Expenses/index.js
+++ js/components/Expenses/index.js
@@ -2,7 +2,11 @@
 
 // libs
 import React from 'react';
+import moment from 'moment';
 import debug from 'debug';
+
+// addons
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 // components
 import Expense from './Expense';
@@ -17,22 +21,42 @@ export default function Expenses(props) {
     people,
   } = props;
 
-  expenses.valueSeq().forEach(log);
+  // Creates date and
+  let listItems = [];
+  let today;
+
+  expenses.map(expense => {
+
+    // Adds a date banner if it's a new day
+    const newToday = moment(expense.date).format("YYYY-MM-DD");
+    if (newToday !== today) {
+      listItems.push(
+        <li className="expense__date-banner" key={newToday}>
+          {moment(expense.date).format("MMMM D, YYYY")}
+        </li>
+      );
+
+      today = newToday;
+    }
+
+    // Creates expenses
+    listItems.push(
+      <Expense
+        key={expense.id}
+        expense={expense}
+        categories={categories}
+        people={people}
+      />
+    );
+  });
 
   return (
     <div className="expenses">
       <h2 className="expenses__heading">List of Expenses</h2>
       <ul className="expenses__list">
-        {expenses.valueSeq().map(expense => {
-          return (
-            <Expense
-              key={expense.id}
-              expense={expense}
-              categories={categories}
-              people={people}
-            />
-          );
-        })}
+        <ReactCSSTransitionGroup transitionName="expense" transitionAppear={true} transitionAppearTimeout={500} transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+          {listItems}
+        </ReactCSSTransitionGroup>
       </ul>
     </div>
   );

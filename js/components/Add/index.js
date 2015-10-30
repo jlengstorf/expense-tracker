184ad804553821174c47659049a84ca335c7b03c
jlengstorf <jason.lengstorf@copterlabs.com>
2015-10-30 19:39:32 +0700

    feat(form): made expense-adding form usable for editing as well

--- js/components/Add/index.js
+++ js/components/Add/index.js
@@ -1,3 +1,4 @@
+/* @flow */
 'use strict';
 
 // libs
@@ -16,9 +17,15 @@ import {dispatch} from '../../dispatcher';
 // debugger
 const log = debug('components/Add');
 
+// types
+import Expense from '../../models/ExpenseModel';
+
 type Props = {
+  expense: ?object,
   categories: object,
   people: object,
+  changeHandler: ?Function,
+  submitHandler: ?Function,
 };
 
 type State = {
@@ -31,37 +38,47 @@ type State = {
 
 export default class Add extends Component<{}, Props, State> {
 
-  constructor(props) {
+  constructor(props: Props): void {
     super(props);
 
-    this.state = {
-      data: Map({
-        date: moment().format('YYYY-MM-DD'),
-        amount: '',
-        vendor: '',
-        categoryID: false,
-        personID: false,
-      }),
-    };
+    // If an expense was supplied, updates the state
+    const expense = this.props.expense || {};
+    if (this.props.expense instanceof Expense) {
+      this.state = {
+        expense: this.props.expense,
+      };
+    } else {
+
+      // Otherwise, initializes with an empty expense
+      this.state = {
+        expense: Map({
+          date: moment().format('YYYY-MM-DD'),
+          amount: '',
+          vendor: '',
+          categoryID: false,
+          personID: false,
+        }),
+      };
+    }
   }
 
   componentWillReceiveProps(nextProps: Props): void {
 
     // Starts by checking that the categoryID is valid
-    if (!this.state.data.categoryID && nextProps.categories.size > 0) {
+    if (!this.state.expense.categoryID && nextProps.categories.size > 0) {
       log('categoryID is false; setting manually.');
-      this.setState(({data}) => ({
-        data: data.set('categoryID', nextProps.categories.first().id),
+      this.setState(({expense}) => ({
+        expense: expense.set('categoryID', nextProps.categories.first().id),
       }));
       log(`categoryID: ${nextProps.categories.first().id}`);
     }
 
     // Checks that the personID is valid
-    if (!this.state.data.personID && nextProps.people.size > 0) {
+    if (!this.state.expense.personID && nextProps.people.size > 0) {
       log('personID is false; setting manually.');
       log('TODO Set the personID to match the logged in user');
-      this.setState(({data}) => ({
-        data: data.set('personID', nextProps.people.first().id),
+      this.setState(({expense}) => ({
+        expense: expense.set('personID', nextProps.people.first().id),
       }));
       log(`personID: ${nextProps.people.first().id}`);
     }
@@ -73,46 +90,61 @@ export default class Add extends Component<{}, Props, State> {
       people,
     } = this.props;
 
+    let changeCB = this._onChange.bind(this);
+    if (typeof this.props.changeHandler === 'function') {
+      log('this.props.changeHandler was set');
+      changeCB = this.props.changeHandler;
+    } else {
+      log('no changeHandler set');
+    }
+
+    let submitCB = this._onSubmit.bind(this);
+    if (typeof this.props.submitHandler === 'function') {
+      log('this.props.submitHandler was set');
+      submitCB = this.props.submitHandler;
+    } else {
+      log('no submitHandler set');
+    }
+
     return (
-      <form onSubmit={this._onSubmit.bind(this)}>
-        <h2>Add an Expense</h2>
+      <form onSubmit={submitCB}>
         <Input
           label="Date"
           name="date"
           pattern="(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))"
-          value={moment(this.state.data.date).format('YYYY-MM-DD')}
-          changeHandler={this._onChange.bind(this)}
+          value={moment(this.state.expense.date).format('YYYY-MM-DD')}
+          changeHandler={changeCB}
         />
         <Input
           label="Amount"
           name="amount"
           type="number"
-          value={this.state.data.amount}
-          changeHandler={this._onChange.bind(this)}
+          value={this.state.expense.amount}
+          changeHandler={changeCB}
         />
         <Input
           label="Vendor"
           name="vendor"
           placeholder="e.g. Acme Corp"
-          value={this.state.data.vendor}
-          changeHandler={this._onChange.bind(this)}
+          value={this.state.expense.vendor}
+          changeHandler={changeCB}
         />
         <Select
           label="Category"
           name="categoryID"
-          value={this.state.data.categoryID}
+          value={this.state.expense.categoryID}
           options={categories}
-          changeHandler={this._onChange.bind(this)}
+          changeHandler={changeCB}
         />
         <Select
           label="Paid By"
           name="personID"
-          value={this.state.data.personID}
+          value={this.state.expense.personID}
           options={people}
-          changeHandler={this._onChange.bind(this)}
+          changeHandler={changeCB}
         />
         <div>
-          <button>Add Expense</button>
+          <input type="submit" value="Add Expense" />
         </div>
       </form>
     );
@@ -121,25 +153,34 @@ export default class Add extends Component<{}, Props, State> {
   _onSubmit(event) {
     event.preventDefault();
 
-    const expense = this.state.data.toObject();
+    const expense = this.state.expense.toObject();
     log(expense);
 
+    // TODO add validation to make sure expenses are legit
     dispatch({
       type: 'expense/create',
       expense: expense,
     });
-
-    return false;
   }
 
   _onChange(event) {
-    const {
+    let {
       name,
       value,
     } = event.target;
 
-    this.setState(({data}) => ({
-      data: data.set(name, value),
+    // Parses the date into a UTC timestamp
+    if (name === 'date') {
+      value = +moment(value, 'YYYY-MM-DD');
+    }
+
+    // Makes sure the amount is saved as a number, not a string
+    if (name === 'amount') {
+      value = +value;
+    }
+
+    this.setState(({expense}) => ({
+      expense: expense.set(name, value),
     }));
   }
 }

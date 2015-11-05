002f100bc3e7aadffe7b3cec19693b7f07f7ef95
jlengstorf <jason.lengstorf@copterlabs.com>
2015-11-05 18:35:56 +0700

    feat(routes): progress on handling URI parameters
    Got the basics working so I can pull in things like group IDs in
    URIs, which is good for bookmarking and shit.
    
    re #3

--- js/router/index.js
+++ js/router/index.js
@@ -21,32 +21,48 @@ import {dispatch} from '../dispatcher';
 const log = debug('router');
 
 /*
- * ## Route Definition
+ * ## Route Handler
  */
 const Router = {
 
   home() {
-    dispatch({
-      type: 'nav/change-view',
-      view: 'home',
-    });
+    updateView('home');
   },
 
   test() {
-    dispatch({
-      type: 'nav/change-view',
-      view: 'test',
-    });
+    updateView('test');
+  },
+
+  testWithParams(request) {
+    log(request);
+    updateView('test', request.params);
   },
 
 };
 
+/*
+ * ## Route Configuration
+ */
 const routeConfig = {
   target: Router,
   '/': 'home',
   '/home': 'home',
-  '/test': 'test',
+  '/test': {
+    '/': 'test',
+    '/:id': 'testWithParams',
+  },
 };
+
+/*
+ * ## Helper Functions
+ */
+function updateView(view: string, params: Object = {}): void {
+  dispatch({
+    type: 'nav/change-view',
+    view: view,
+    params: params,
+  });
+}
 
 export default Router;
 export {routeConfig};

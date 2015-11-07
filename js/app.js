fda71aaa1731641bc4058fe9b9279543ca54e614
jlengstorf <jason.lengstorf@copterlabs.com>
2015-11-07 17:29:01 +0700

    feat(comments): added literate programming-style comments

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
+/*
+ * # app.js
+ * This is the script that starts the app.
+ *
+ * @flow
+ */
 'use strict';
 
-//libs
+/*
+ * ## JS Dependencies
+ */
+
+// libs
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 // components
 import ExpenseTracker from './components/ExpenseTracker';
 
-// styles (via webpack)
+/*
+ * ## Styles
+ * Stylesheets are included like JavaScript files because webpack is crafty.
+ */
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../css/main.css';
 
+/*
+ * # Start the Application
+ * We attach the ExpenseTracker component to the DOM, which is where the app
+ * officially starts.
+ */
 ReactDOM.render(
   <ExpenseTracker />,
   document.getElementById('expense-tracker')
